61981b9d24e58423482b4fab78be74a41b7ff7d2
Sergei Ivanov <swordie.ser@gmail.com>
2022-09-11 17:41:53 +0300

    test: add test-suite and changed main

--- tests/src/test/java/Main.java
+++ tests/src/test/java/Main.java
@@ -3,7 +3,7 @@ import org.testng.TestNG;
 public class Main {
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
-        testNG.setTestClasses(new Class[]{FtpClientTest.class});
+        testNG.setTestClasses(new Class[]{FtpClientTest.class, CommandsTest.class});
         testNG.run();
     }
 }
